23ec8b14be2fcbbe431a32c52559348b990f1cae
matthieu0guim <m.guimard@solaris-bi.com>
2021-10-27 12:22:30 +0200

    MaJ des docstring du controller

--- src/controller.py
+++ src/controller.py
@@ -11,8 +11,11 @@ class AppController:
     @classmethod
     def create_tournament(cls, attrs):
         """Create the tournament in database
-        attrs are the attributes collected by the view
-        """
+
+        Args:
+            attrs (dictionnary): attributes collected by the view
+        """        """"""
+
         id = Tournament.set_tournament_id()
         attrs["id"] = id
         Tournament.create(attrs)
@@ -20,7 +23,10 @@ class AppController:
     @classmethod
     def create_player(cls, attrs):
         """Create a new player in database
-        attrs are the attributes collected by the view"""
+
+        Args:
+            attrs (dictionnary): attrs are the attributes collected by the view
+        """
         id = Tournament.set_player_id()
         attrs["id"] = id
         Player.create(attrs)
@@ -28,20 +34,29 @@ class AppController:
     @classmethod
     def generate_tour(cls, tournament_id_user_choice):
         """Generate a new round for an ongoing tournament
-        The wished tournament is entered with the attribute tournament_id_user_choice
-        """
+
+        Args:
+            tournament_id_user_choice (integer): id of the concerned tournament
+
+        Returns:
+            [list]: a list containing all machs of the round
+        """        """"""
+        
         round = Tournament.generate_round(tournament_id_user_choice)
         return round
 
     @classmethod
     def set_tour_results(cls, matchs_results, round_id, tournament_id_user_choice,
                          match_id, player_one_score=None, player_two_score=None):
-        """Allowes to register a round result
-        matchs_results is a list of matches
-        round_id is the concerned round
-        tournament_id_user_choice is the concerned tournament
-        match_id is the id of a match in the round
-        player_one_score/player_two_score are the resuls of the match refered by match_id
+        """[summary]
+
+        Args:
+            matchs_results (list): a list of matches
+            round_id (integer): the concerned round
+            tournament_id_user_choice (integer): the concerned tournament
+            match_id (integer): id of a match in the round
+            player_one_score (float, optional): resuls of the match refered by match_id. Defaults to None.
+            player_two_score (float, optional): resuls of the match refered by match_id. Defaults to None.
         """
         Tournament.enter_results(tournament_id_user_choice, round_id,
                                  match_id, player_one_score,
@@ -51,7 +66,12 @@ class AppController:
     @classmethod
     def get_provisional_ranking(cls, tournament_id_user_choice):
         """Used to send the ranking of a tournament
-        It returns a sorted by tournament_score list for the wished tournament
+
+        Args:
+            tournament_id_user_choice (integer): id of thre  wished tournament
+
+        Returns:
+            list: sorted by tournament_score list for the wished tournament
         """
         players = Tournament.get_players(tournament_id_user_choice)
         return players
@@ -59,9 +79,16 @@ class AppController:
     @classmethod
     def get_player_info(cls, player_choice=None):
         """send either informations about every players of the data base
-        If the player_choice is not specified, all players in database are returned
-        If the player_choice is specified with its id it only returns the concerned one
-         """
+
+        Args:
+            player_choice (integer, optional): id of the wished player
+
+        Returns:
+            if player-choice specified:
+                list: contains all informations about the player
+            else:
+                list: list all list where each one is about a player
+        """
         if not player_choice:
             players = Tournament.get_all_players()
             return players
@@ -71,13 +98,23 @@ class AppController:
     @classmethod
     def set_player_elo(cls, player, new_elo):
         """Allows the user to update a player's elo
-        It takes the id of the player and the new value as arguments
+
+        Args:
+            player (integer): id of the player
+            new_elo (integer): the new value of elo
         """
         Tournament.change_player_elo(player, new_elo)
 
     @classmethod
     def get_game_list(cls, tournament_id):
-        """Return the games list of the ongoing round for the specified tournament"""
+        """Return the games list of the ongoing round for the specified tournament
+
+        Args:
+            tournament_id (integer): id of the wished tournament
+
+        Returns:
+            list: list of the ongoing round
+        """
         return Tournament.get_game_list(tournament_id)
 
     @classmethod
@@ -86,12 +123,23 @@ class AppController:
                    sorting=None,
                    round_choice=None,
                    choosing=None):
-        """Return informations about a required subject
-        5 kind of reports are possible:
-                - All players in database sorted alphabetically or by rank
-                - The list of all tournaments in database
-                - The list of player in a tournament sorted alphabetically or by rank
-                - The list of played and ongoing rounds in a tournament
-                - The list of games in a round for a specified tournament
+        """[summary]
+
+        Args:
+            choice (int, optional): number of the command. Defaults to 0.
+            tournament_choice (integer, optional): id of the wished tournament. Defaults to None.
+            sorting (string, optional): choice of sorting, alphabetical or rank. Defaults to None.
+            round_choice (integer, optional): id of the wished round. Defaults to None.
+            choosing (Boolean, optional): used to delete the list of all possible games when the programm
+                                          uploads all tournaments in base. Defaults to None.
+
+        Returns:
+            list or dictionnary: depends of the user choice.
+                                5 kind of reports are possible:
+                                - All players in database sorted alphabetically or by rank
+                                - The list of all tournaments in database
+                                - The list of player in a tournament sorted alphabetically or by rank
+                                - The list of played and ongoing rounds in a tournament
+                                - The list of games in a round for a specified tournament
         """
         return Tournament.get_report(choice, tournament_choice, sorting, round_choice, choosing)
